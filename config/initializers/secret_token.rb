33185ba5e7f417460db64543d51e48125f9c9ef0
gregmalcolm <greg@gregmalcolm.com>
2013-09-01 14:57:25 -0400

    Blank rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DynatreeEmberExercise::Application.config.secret_key_base = 'f5204a7ac368dfdd3bf3f08e8a39d61cbdfb05b8e7a57e80de381cccfb00fbb6461e61142a85bd2dccfa705a333d7d48b857b61dc12e1f69e8140f4229b0f4b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DynatreeEmberExercise::Application.config.session_store :cookie_store, key: '_dynatree-ember-exercise_session'
